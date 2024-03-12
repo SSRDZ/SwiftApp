dac9e7b342d2d657049019f9b507f84f4472cf3f
SSRDZ <143786667+SSRDZ@users.noreply.github.com>
2024-03-11 19:44:03 -0700

    drtd

--- ReservationApp/ReservationApp/ContentView.swift
+++ ReservationApp/ReservationApp/ContentView.swift
@@ -11,17 +11,7 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+    "Item at \(item.timestamp!, formatter: itemFormatter)")
                     } label: {
                         Text(item.timestamp!, formatter: itemFormatter)
                     }
