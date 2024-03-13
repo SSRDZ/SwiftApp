5756bff20eacf119a66b9d1ea6030de558f476e0
SSRDZ <143786667+SSRDZ@users.noreply.github.com>
2024-03-12 21:55:12 -0700

    link screen

--- ReservationApp/ReservationApp/home.swift
+++ ReservationApp/ReservationApp/home.swift
@@ -9,55 +9,46 @@ import SwiftUI
 
 struct home: View {
     //var profile = Profile()
+    @State var inReserve = false
+    
     var body: some View {
-        
-        VStack{
-            
-            
-                
-            
-            Text("profile.name")
-                .font(Font.system(size: 40, design: .default))
-                .padding(.bottom,1 )
+        NavigationView{
+            VStack{
+                Text("profile.name")
+                    .font(Font.system(size: 40, design: .default))
+                    .padding(.bottom,1 )
                 
                 Spacer()
-            
-            Image("brs")
-                .resizable()
-                .frame(width: 300.0, height: 185.25)
-                .cornerRadius(30)
+                
+                Image("brs")
+                    .resizable()
+                    .frame(width: 300.0, height: 185.25)
+                    .cornerRadius(30)
                 Spacer()
-
-            
-            Text("welcome to MUM")
-                .padding(.bottom, 100
-                )
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 
-            Button("start             ",action: {Complete()}) //อย่าลืมเปลี่ยนเป็นฟังชันไซน์อิน
-                .buttonStyle(.bordered)
-                .foregroundColor(.white)
-                .background(.brown)
-                .cornerRadius(10)
-                .padding(.bottom, 100)
-            
-               
-          
-
-
-
-
-            
                 
-
-            
+                Text("welcome to MUM")
+                    .padding(.bottom, 100
+                    )
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                NavigationLink(destination: ReserveScreen(), isActive: $inReserve){}
+                Button("start             ",action: {Complete()}) //อย่าลืมเปลี่ยนเป็นฟังชันไซน์อิน
+                    .buttonStyle(.bordered)
+                    .foregroundColor(.white)
+                    .background(.brown)
+                    .cornerRadius(10)
+                    .padding(.bottom, 100)
+                
+            }
+            .padding(45)
+            .background(Color("skin"))
         }
-        .padding(45)
-        .background(Color("skin"))
+        .navigationBarHidden(true)
     }
     func Complete()
     {
-        
+        inReserve = true
     }
 
 }
